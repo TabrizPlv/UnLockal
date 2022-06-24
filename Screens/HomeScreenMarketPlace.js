ccc922fd813f5eb27395e5739ce747e0f6025e62
bryan <bryanong1589@gmail.com>
2022-06-24 21:34:59 +0800

    Added a button to Marketplace

--- Screens/HomeScreenMarketPlace.js
+++ Screens/HomeScreenMarketPlace.js
@@ -1,13 +1,18 @@
 import * as React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/core";
 
 export default function HomeScreenMarketPlace({ navigation }) {
+  const navigationn = useNavigation();
   return (
     <View style={styles.container}>
       <View style={{ marginHorizontal: 10 }}>
         <Text style={styles.message}>
           Marketplace coming soon!
         </Text>
+        <TouchableOpacity style = {styles.button} onPress = {() => navigationn.navigate("EmptyStoreTemplate")}>
+          <Text>Edit your store instead</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -15,12 +20,19 @@ export default function HomeScreenMarketPlace({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "white",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    paddingHorizontal: 10
   },
   message: {
     color: "black",
+    padding: 20,
   },
+  button: {
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: 'teal',
+    borderRadius: 10
+  }
 });
