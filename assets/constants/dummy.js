07c8d3090e6068629b4b0c3b04ef9d0aac423d44
bryan <bryanong1589@gmail.com>
2022-06-26 22:38:19 +0800

    test

--- assets/constants/dummy.js
+++ assets/constants/dummy.js
@@ -2,13 +2,13 @@ import assets from "./assets";
 
 const NFTData = [
   {
-    id: "NFT-01",
-    name: "Abstracto #312",
-    creator: "Putri Intan",
+    id: "01",
+    name: "Tobyato Designs",
+    creator: "Tobyato",
     price: 4.25,
     description:
       "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural.",
-    image: assets.nft01,
+    image: assets.t,
     bids: [
       {
         id: "BID-11",
@@ -34,9 +34,9 @@ const NFTData = [
     ],
   },
   {
-    id: "NFT-02",
-    name: "Green Coins",
-    creator: "Siti Nurhaliza",
+    id: "02",
+    name: "Green Plant Finance",
+    creator: "Tabriz Phalavi",
     price: 7.25,
     description:
       "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
@@ -52,9 +52,9 @@ const NFTData = [
     ],
   },
   {
-    id: "NFT-03",
-    name: "NFT coins race",
-    creator: "Elisabeth aho",
+    id: "03",
+    name: "Haste Gaming",
+    creator: "Bryan Ong",
     price: 95.25,
     description:
       "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
@@ -77,9 +77,9 @@ const NFTData = [
     ],
   },
   {
-    id: "NFT-04",
-    name: "Nifty NFT",
-    creator: "Putri Intan",
+    id: "04",
+    name: "Hola Bali",
+    creator: "Devanshu Bisht",
     price: 54.25,
     description:
       "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural.Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur.",
@@ -116,9 +116,9 @@ const NFTData = [
     ],
   },
   {
-    id: "NFT-05",
+    id: "05",
     name: "Colorful circles",
-    creator: "David doe",
+    creator: "Justin Toh",
     price: 10.25,
     description:
       "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural.",
@@ -134,9 +134,9 @@ const NFTData = [
     ],
   },
   {
-    id: "NFT-06",
-    name: "Black box model",
-    creator: "Leo Messi",
+    id: "06",
+    name: "Black Box Music",
+    creator: "Benjamin Wee",
     price: 20.25,
     description:
       "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
@@ -180,9 +180,9 @@ const NFTData = [
     ],
   },
   {
-    id: "NFT-07",
-    name: "Abstracto soulful art",
-    creator: "Victor de la Cruz",
+    id: "07",
+    name: "Victor's Jewelry",
+    creator: "Victor Tham",
     price: 18.25,
     description:
       "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis. Ullamcorper platea penatibus ornare egestas nulla ligula hendrerit nisl suscipit sociosqu maximus, tincidunt aptent habitant purus pharetra ultrices dapibus laoreet nisi lacinia. Porta malesuada netus vel sapien conubia porttitor aliquam ut pretium ante litora molestie senectus magna egestas sociosqu, eget aliquet fames pharetra felis posuere varius fringilla quisque in arcu montes eu ullamcorper.",
