666a8e281ecfd8fc3be606ab0de1bcecd157fc62
TabrizPlv <tabrizpahlavi@gmail.com>
2022-06-27 15:17:04 +0800

    Merged some client requests. Removed redundant UserListing

--- src/ClientRequests/getUserDetails.js
+++ src/ClientRequests/getUserDetails.js
@@ -2,22 +2,22 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { getUserId } from "../User-Info-Functions";
 
-export async function handleGetUserListing() {
+export async function handleGetUserDetails() {
   const userId = await getUserId("userToken");
-  const [userListing, setUserListing] = useState('');
-  const url = "http://192.168.86.235:3001/api/user/" + userId + "/get-listings";
+  const [userDetails, setUserDetails] = useState('');
+  const url = "http://192.168.86.235:3001/api/user/" + userId + "/get-userdetails";
 
   useEffect(() => {
-    const fetchAndSetUserListing = async () => {
+    const fetchAndSetUserDetails = async () => {
       await axios
         .get(url)
         .then((response) => {
           const data = response.data;
-          setUserListing(data);
+          setUserDetails(data);
         })
         .catch((error) => console.log(error));
     };
-    fetchAndSetUserListing();
+    fetchAndSetUserDetails();
   }, []);
-  return userListing;
+  return userDetails;
 }
